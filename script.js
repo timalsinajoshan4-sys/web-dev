58bea309c125be10dac06e52bac3f2fe569ff762
joshan <timalsinajoshan4@gmail.com>
2026-09-13 11:23:23 +0545

    js-class-1st

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("External JavaScript loaded");
+const x = "Susan Baddie"; //- const is a variable that cannot be reassigned-/
+console.log(x);
+const a = 5;
+const b = 10;
+const sum = a + b;
+console.log("Sum:", sum);
+const product = a * b;
+console.log("Product:", product);
+const remainder = b % a;
+console.log("Remainder:", remainder);
+
+// If x=7, y=8, z=9. Find if they are even number or odd number.
